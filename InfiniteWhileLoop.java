3e005fb6aba5702b32999d0a0f4870673e1370e7
KARTHIKEYAN.M <vh12592_mech22@velhightech.com>
2025-02-02 14:17:58 +0530

    Add files via upload
    While loop code to print a string infinite times.

---
+++ InfiniteWhileLoop.java
@@ -0,0 +1,8 @@
+public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+
+        while(true)
+        System.out.println("This Problem prints infinite While Loop ");
+
+    }
+}
