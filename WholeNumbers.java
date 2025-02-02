802eabb6f37932c77dbcd854bdf02072bdf4374f
KARTHIKEYAN.M <vh12592_mech22@velhightech.com>
2025-02-02 22:09:48 +0530

    Add files via upload
    This program prints the first 10 whole numbers and prints the sum of 10 whole numbers.

---
+++ WholeNumbers.java
@@ -0,0 +1,13 @@
+class WholeNumbers{
+    public static void main(String[] args)
+    {
+     int sum = 0;
+     System.out.println("First 10 Whole numbers:");
+        for (int i = 0; i <= 10; i++)
+        {
+           sum = sum + i;  
+           System.out.println(i);
+        }
+      System.out.println("sum of first 10 Whole number: "+sum);
+    }
+}
