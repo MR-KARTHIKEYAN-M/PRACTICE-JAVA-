c00e7ef9944216affaaa2d8f9dea68a90d0bad62
KARTHIKEYAN.M <vh12592_mech22@velhightech.com>
2025-02-02 14:14:21 +0530

    Add files via upload
    A simple  Do while loop to print a string Infinite  times.

---
+++ InfiniteDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class InfiniteDoWhileLoop {
+    public static void main(String[] args) {
+
+        do{
+            System.out.println("This Problem prints infinite Do While Loop ");
+        }
+        while(true);
+
+    }
+}
